470ea3c8db2637859cd3960f2d7a86a00e44a923
bhansali-arham <arham.bhansali.che20@itbhu.ac.in>
2023-11-23 12:31:06 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+// eslint-disable-next-line
+import { Card, CardContent,CardHeader,Grid,Grow, Typography, Container ,Button,ButtonBase, CardMedia} from '@mui/material';
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+    
+    <Grow in>
+    <Container maxWidth="xl"  className="container">
+      <Typography variant = 'h1' className="header" >
+      ALGOXPLORE
+      </Typography >
+
+    
+        <Card  className="card" elevation={0} >
+      <img  src={require(`./assets/sudoku_solver.jpg`)} className="img-responsive" alt='credit-card.png' />
+
+      <CardHeader  varant ='h3' title="Sudoku Solver" />
+            <CardContent className='content'>
+            A sudoku solver  which lets user make their own sudoku and
+also solves the sudoku using backtracking algorithm
+            </CardContent>
+
+        <Button variant="contained" href='https://unseen703.github.io/Sudoku-Solver/'>
+        Solve Puzzle
+        </Button>
+        
+      </Card>
+
+        <Card  className="card" elevation={0} >
+      <img  src={require(`./assets/sorting.38780fe7a3d79244394d.jpg`)} className="img-responsive" alt='Sorting_visualizer.png' />
+
+      <CardHeader  varant ='h3' title="Sorting Visualizer" />
+            <CardContent className='content'>
+            A Sorting visualizer to visualize and understand the sorting algorithms and understand the time complexity of them.
+            </CardContent>
+
+        <Button variant="contained" href='https://unseen703.github.io/Sorting_Visualizer/'>
+        Visualize
+        </Button>
+        
+      </Card>
+
+       
+      <Card   className="card" elevation={0} >
+      <img  src={require(`./assets/tictactoe.png`)} className="img-responsive" alt='credit-card.png' />
+      <CardHeader  varant ='h3' title="Tic Tac Toe" />
+            <CardContent className='content'>
+            A user-friendly web page demonstrating Tic Tac Toe Game using the minimax
+algorithm to build an unbeatable tic tac toe.
+            </CardContent>
+        <Button variant ='contained'  href= "https://unseen703.github.io/Tic-Tac-Toe/">
+          Let's Play
+        </Button>
+        </Card>
+      <Card   className="card" elevation={0} >
+      <img  src={require(`./assets/snack_mania.jpeg`)} className="img-responsive" alt='credit-card.png' />
+      <CardHeader  varant ='h3' title="Snack Mania" />
+            <CardContent className='content'>
+             A snack Mania application lets user to play around with snack using strok of the arrow keys
+            </CardContent>
+        <Button variant ='contained'  href= "https://unseen703.github.io/SnakeGame/">
+          Let's Play
+        </Button>
+        </Card>
+
+
+
+    </Container>
+    </Grow>
+    </>
+  );
+}
+
+export default App;
